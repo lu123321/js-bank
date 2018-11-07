040d4fd20c2b5d0b240a7e856c765469792be7d1
git <191762443@qq.com>
2018-11-07 20:00:34 +0800

    保险消费着

---
+++ src/main/java/com/example/jsconsumerinsurance/JsConsumerInsuranceApplication.java
@@ -0,0 +1,14 @@
+package com.example.jsconsumerinsurance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class JsConsumerInsuranceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsConsumerInsuranceApplication.class, args);
+    }
+}
